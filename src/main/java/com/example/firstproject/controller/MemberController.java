fe3dab44216004fc0bb45e4c4bf5957c01777677
junHyeong <pjh971212@gmail.com>
2024-01-31 14:50:10 +0900

    feat:complete chapter5

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,10 +3,16 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
+@Slf4j
 @Controller
 public class MemberController {
 
@@ -16,15 +22,32 @@ public class MemberController {
         this.memberRepository = memberRepository;
     }
 
-    @GetMapping("/member/new")
+    @GetMapping("/signup")
     public String newMemberForm(){
-        return "member/new";
+        return "members/new";
     }
 
     @PostMapping("/join")
     public String createMember(MemberForm memberForm){
+        log.info(memberForm.toString());
         Member member = memberForm.toEntity();
-        memberRepository.save(member);
+        log.info(member.toString());
+        Member saved = memberRepository.save(member);
+        log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        ArrayList<Member> memberEntity = memberRepository.findAll();
+        model.addAttribute("members", memberEntity);
+        return "members/index";
     }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,10 +5,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor // 기본 생성자 추가 어노테이션
+@ToString
 @Entity
 public class Article {
-
     @Id
     @GeneratedValue
     private Long id;
@@ -16,25 +21,4 @@ public class Article {
     private String title;
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    public Article() {
-
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
-
-
 }
