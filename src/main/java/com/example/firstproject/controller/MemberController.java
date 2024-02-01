ea20f24ae2d044a4d6284ac9c0460972e33de155
junHyeong <pjh971212@gmail.com>
2024-02-01 10:00:38 +0900

    feat:complete chapter 8

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,9 +6,11 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -68,4 +70,19 @@ public class MemberController {
 
         return "redirect:/members" + member.getId();
     }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        Member target = memberRepository.findById(id).orElse(null);
+
+        if(target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다.");
+        }
+
+        return "redirect:/members";
+    }
+
+
+
 }
--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Member {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
