5b2ee8aa6c300c37b1137e5a3a80a4431d9b195c
junHyeong <pjh971212@gmail.com>
2024-02-01 17:48:08 +0900

    feat:complete chapter 13

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
 INSERT INTO article(title, content) VALUES ('가가가가', '1111');
 INSERT INTO article(title, content) VALUES ('나나나나', '2222');
 INSERT INTO article(title, content) VALUES ('다다다다', '3333');
+
 INSERT INTO member(email, password) VALUES ('abc@abc.com', '1111');
 INSERT INTO member(email, password) VALUES ('qwe@abc.com', '2222');
 INSERT INTO member(email, password) VALUES ('zxc@abc.com', '3333');
